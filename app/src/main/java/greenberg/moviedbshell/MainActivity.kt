5eba8ba4f59d70ada00be7102ff9ddeeadcb9b1b
Sam Greenberg <11greenberg.sam@gmail.com>
2018-04-09 18:58:30 -0400

    Lots of stuff?
    Should've kept better track.

--- app/src/main/java/greenberg/moviedbshell/MainActivity.kt
+++ app/src/main/java/greenberg/moviedbshell/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.PictureInPictureParams
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.CollapsingToolbarLayout
+import android.support.v4.widget.NestedScrollView
 import android.util.Log
 import android.util.Rational
 import android.widget.ImageView
@@ -21,15 +23,25 @@ class MainActivity : AppCompatActivity() {
     private var compositeDisposable: CompositeDisposable = CompositeDisposable()
     private lateinit var outputTextView: TextView
     private lateinit var posterImageView: ImageView
+    private lateinit var collapsingToolbarLayout: CollapsingToolbarLayout
+    private lateinit var scrollView: NestedScrollView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        outputTextView = findViewById(R.id.output)
-        posterImageView = findViewById(R.id.poster)
+        //outputTextView = findViewById(R.id.output)
+        //posterImageView = findViewById(R.id.poster)
 
         movieService = RetrofitHelper().getMovieService()
+
+        collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar)
+        collapsingToolbarLayout.title = "Testing"
+        collapsingToolbarLayout.setExpandedTitleColor(resources.getColor(android.R.color.transparent))
+
+        posterImageView = findViewById(R.id.image)
+        scrollView = findViewById(R.id.scroll)
+        outputTextView = findViewById(R.id.output)
 
         requestMovie()
     }
@@ -52,19 +64,29 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayInfo(movieResponse: MovieResponse) {
         Log.d("Hmm", movieResponse.let { "${it.originalTitle}" })
-        outputTextView.text = movieResponse.originalTitle
+        //outputTextView.text = movieResponse.originalTitle
+        outputTextView.text = movieResponse.let { "${it.overview}" }
+
     }
 
     private fun fetchPoster(movieResponse: MovieResponse) {
-        Log.d("Hmm", movieResponse.let { "${it.posterPath}" })
-        movieResponse.posterPath?.let {
+        Log.d("Hmm", movieResponse.let { "${it.backdropPath}" })
+        movieResponse.backdropPath?.let {
+            Glide.with(this)
+                    .load(buildPosterURL(it))
+                    .apply { RequestOptions().centerInside() }
+                    .into(posterImageView)
+        }
+        /*movieResponse.posterPath?.let {
             Glide.with(this)
                     .load(buildPosterURL(it))
                     .apply { RequestOptions().centerCrop() }
                     .into(posterImageView)
-        }
+        }*/
     }
 
     //TODO: replace this with string resources
-    private fun buildPosterURL(endurl: String) = "https://image.tmdb.org/t/p/original/$endurl"
+    private fun buildPosterURL(endurl: String) = "https://image.tmdb.org/t/p/original$endurl"
+
+    private fun buildBackgroundPosterURL(endurl: String) = "https://image.tmdb.org/t/p/original$endurl"
 }
