8b1209dc8e3d70092854328d85b8217d49c0da7c
Sam Greenberg <11greenberg.sam@gmail.com>
2018-04-18 23:14:39 -0400

    More clean up
    -Adding keys for ms app center integration
    -integrating ms app center
    -fixing git ignores for reasons
    -trying to sort out gradlew issues
    -added application class for ms app center
    
    I have literally no idea what I'm doing sometimes, gradlew build doesn't
    work afaik.

---
+++ app/src/main/java/greenberg/moviedbshell/ZephyrrApplication.kt
@@ -0,0 +1,21 @@
+package greenberg.moviedbshell
+
+import MS_APPCENTER_KEY
+import android.app.Application
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.AppCenter
+
+
+
+class ZephyrrApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCenter.start(this,
+                MS_APPCENTER_KEY,
+                Analytics::class.java,
+                Crashes::class.java)
+    }
+}
---
+++ app/src/main/java/greenberg/moviedbshell/keys/Constants.kt
@@ -0,0 +1,1 @@
+const val MS_APPCENTER_KEY = "a55fef2b-4bab-4a81-b43a-d0c4ad66412e"
