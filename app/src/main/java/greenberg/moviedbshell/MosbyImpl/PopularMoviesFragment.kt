2b49c42dd1612064dbe18550cfe0db6c73427796
sag343 <sag343@drexel.edu>
2018-04-21 17:08:19 -0400

    I think loading and hiding views works now?
    This could be buggy.

--- app/src/main/java/greenberg/moviedbshell/MosbyImpl/PopularMoviesFragment.kt
+++ app/src/main/java/greenberg/moviedbshell/MosbyImpl/PopularMoviesFragment.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import com.hannesdorfmann.mosby3.mvp.MvpFragment
 import greenberg.moviedbshell.Models.PopularMoviesModels.PopularMovieResponse
 import greenberg.moviedbshell.PopularMovieAdapter
@@ -22,6 +23,7 @@ class PopularMoviesFragment :
     private lateinit var linearLayoutManager: LinearLayoutManager
     private var popularMovieAdapter: PopularMovieAdapter? = null
     private var popularMovieRefresher: SwipeRefreshLayout? = null
+    private var popularMovieLoadingBar: ProgressBar? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.popular_movies_layout, container, false)
@@ -32,6 +34,8 @@ class PopularMoviesFragment :
 
         popularMovieRecycler = activity?.findViewById(R.id.popularMovieRecycler)
         popularMovieRefresher = activity?.findViewById(R.id.popularMovieRefresher)
+        popularMovieLoadingBar = activity?.findViewById(R.id.popularMovieProgressBar)
+        popularMovieRefresher?.setOnRefreshListener(this)
 
         //TODO: look into proper context for this; i.e. application or base
         linearLayoutManager = LinearLayoutManager(activity)
@@ -47,6 +51,9 @@ class PopularMoviesFragment :
 
     override fun showLoading(pullToRefresh: Boolean) {
         Log.w("Testing", "Show Loading")
+        popularMovieRefresher?.visibility = View.GONE
+        popularMovieRecycler?.visibility = View.GONE
+        popularMovieLoadingBar?.visibility = View.VISIBLE
         presenter.loadPopularMovies(pullToRefresh)
     }
 
@@ -59,6 +66,9 @@ class PopularMoviesFragment :
     override fun showMovies() {
         Log.w("Testing", "Showing Movies")
         popularMovieRefresher?.isRefreshing = false
+        popularMovieLoadingBar?.visibility = View.GONE
+        popularMovieRefresher?.visibility = View.VISIBLE
+        popularMovieRecycler?.visibility = View.VISIBLE
     }
 
     override fun showError(throwable: Throwable, pullToRefresh: Boolean) {
@@ -69,6 +79,8 @@ class PopularMoviesFragment :
     override fun onRefresh() {
         Log.w("Testing", "On Refresh")
         showLoading(true)
+        popularMovieAdapter?.popularMovieList = null
+        popularMovieAdapter?.notifyDataSetChanged()
     }
 
 }
