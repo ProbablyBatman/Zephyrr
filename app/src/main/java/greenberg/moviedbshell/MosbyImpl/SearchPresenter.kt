58f4556ec2e8345ea297fa25e66285bc3d1617d3
Sam Greenberg <11greenberg.sam@gmail.com>
2018-06-24 13:04:10 -0400

    Move utility functions from viewholders to presenters

--- app/src/main/java/greenberg/moviedbshell/MosbyImpl/SearchPresenter.kt
+++ app/src/main/java/greenberg/moviedbshell/MosbyImpl/SearchPresenter.kt
@@ -2,11 +2,17 @@ package greenberg.moviedbshell.MosbyImpl
 
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter
 import greenberg.moviedbshell.Models.SearchModels.SearchResultsItem
+import greenberg.moviedbshell.R
 import greenberg.moviedbshell.RetrofitHelpers.RetrofitHelper
+import greenberg.moviedbshell.ViewHolders.SearchResultsAdapter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import java.text.SimpleDateFormat
 
 class SearchPresenter : MvpBasePresenter<ZephyrrSearchView>() {
 
@@ -84,5 +90,38 @@ class SearchPresenter : MvpBasePresenter<ZephyrrSearchView>() {
                             view.showError(throwable, false)
                     }
                 })
+    }
+
+    fun fetchPoster(cardItemPosterView: ImageView, item: SearchResultsItem) {
+        //Load poster art
+        when (item.mediaType) {
+            SearchResultsAdapter.MEDIA_TYPE_MOVIE, SearchResultsAdapter.MEDIA_TYPE_TV -> {
+                item.posterPath
+            }
+            SearchResultsAdapter.MEDIA_TYPE_PERSON -> {
+                item.profilePath
+            }
+            else -> {
+                //Don't fetch poster if there is no poster art
+                return
+            }
+        }?.let {
+            Glide.with(cardItemPosterView)
+                    //TODO: potentially hacky way to get context
+                    .load(cardItemPosterView.context.getString(R.string.poster_url_substitution, it))
+                    .apply { RequestOptions().centerCrop() }
+                    .into(cardItemPosterView)
+        }
+    }
+
+    fun processReleaseDate(releaseDate: String): String {
+        return if (releaseDate.isNotBlank()) {
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd")
+            val date = inputFormat.parse(releaseDate)
+            val outputFormat = SimpleDateFormat("MM/dd/yyyy")
+            outputFormat.format(date)
+        } else {
+            return ""
+        }
     }
 }
--- app/src/main/java/greenberg/moviedbshell/ViewHolders/PopularMovieViewHolder.kt
+++ app/src/main/java/greenberg/moviedbshell/ViewHolders/PopularMovieViewHolder.kt
@@ -7,25 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import greenberg.moviedbshell.Models.PopularMoviesModels.PopularMovieResultsItem
 import greenberg.moviedbshell.MosbyImpl.PopularMoviesPresenter
 import greenberg.moviedbshell.R
-import java.text.SimpleDateFormat
 
 class PopularMovieAdapter(var popularMovieList: MutableList<PopularMovieResultsItem?> = mutableListOf(),
-                          var popularMoviesPresenter: PopularMoviesPresenter) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+                          private val presenter: PopularMoviesPresenter) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is PopularMovieViewHolder) {
             val currentItem = popularMovieList.getOrNull(position)
             //todo: load posters and have like, placeholders
             holder.cardItemTitle.text = currentItem?.title
-            holder.cardItemReleaseDate.text = currentItem?.releaseDate?.let { processReleaseDate(it) }
+            holder.cardItemReleaseDate.text = currentItem?.releaseDate?.let { presenter.processReleaseDate(it) }
             holder.cardItemOverview.text = currentItem?.overview
-            popularMovieList[position]?.let { fetchPoster(holder.cardItemPosterImage, it) }
-            holder.cardItem.setOnClickListener { popularMoviesPresenter.onCardSelected(currentItem?.id ?: -1) }
+            popularMovieList[position]?.let { presenter.fetchPoster(holder.cardItemPosterImage, it) }
+            holder.cardItem.setOnClickListener { presenter.onCardSelected(currentItem?.id ?: -1) }
         }
     }
 
@@ -33,29 +30,6 @@ class PopularMovieAdapter(var popularMovieList: MutableList<PopularMovieResultsI
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return PopularMovieViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.popular_movie_card, parent, false))
-    }
-
-    private fun fetchPoster(cardItemPosterView: ImageView, item: PopularMovieResultsItem) {
-        //Load poster art
-        item.posterPath?.let {
-            Glide.with(cardItemPosterView)
-                    .load(cardItemPosterView.context.getString(R.string.poster_url_substitution, it))
-                    .apply { RequestOptions().centerCrop() }
-                    .into(cardItemPosterView)
-        }
-    }
-
-    //TODO: probably make sure every date is like this?
-    //there has to be a better way to do this
-    private fun processReleaseDate(releaseDate: String): String {
-        return if (releaseDate.isNotBlank()) {
-            val inputFormat = SimpleDateFormat("yyyy-MM-dd")
-            val date = inputFormat.parse(releaseDate)
-            val outputFormat = SimpleDateFormat("MM/dd/yyyy")
-            outputFormat.format(date)
-        } else {
-            return ""
-        }
     }
 
     class PopularMovieViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
